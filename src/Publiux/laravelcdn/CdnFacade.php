5d3ec6444ac5d62dfcb6c5d4868aa0d113d990e3
Juho Choi <mail@ool.so>
2018-07-24 16:45:55 +0900

    prevent double slashes when bypass is true on generateUrl method

--- src/Publiux/laravelcdn/CdnFacade.php
+++ src/Publiux/laravelcdn/CdnFacade.php
@@ -109,7 +109,7 @@ class CdnFacade implements CdnFacadeInterface
         // if the package is surpassed, then return the same $path
         // to load the asset from the localhost
         if (isset($this->configurations['bypass']) && $this->configurations['bypass']) {
-            return Request::root().'/'.$path;
+            return Request::root().(!starts_with($path, '/') ? '/' : '').$path;
         }
 
         if (!isset($path)) {
